351ee0f25912bd7c78b2bc3c69a6049ae9ebbb7f
arthropodas <asifptb456@gmail.com>
2024-07-24 15:31:40 +0530

    resolved conflict in the rebase

--- shoppie-client/src/components/Home/Header.tsx
+++ shoppie-client/src/components/Home/Header.tsx
@@ -95,7 +95,7 @@ const Header: React.FC = () => {
             <FiShoppingBag className="w-5 h-5 mr-2" />
             <span className="hidden sm:inline">Become a seller</span>
           </button>
-          <button>
+          <button >
             <Themeswitcher />
           </button>
         </span>
